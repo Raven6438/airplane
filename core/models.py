5053e05f3269d56523c95393012fb05bc79e25ff
Artur <halilov.artur@gmail.com>
2024-01-14 21:51:30 +0500

    #4 Написал кастом модель пользователя, реализовал роли

--- core/models.py
+++ core/models.py
@@ -1,3 +1,8 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+from core import consts
+
+
+class User(AbstractUser):
+    role = models.CharField('Роль', max_length=255, choices=consts.ROLE_CHOICES)
